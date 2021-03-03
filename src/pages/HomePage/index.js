082b986b3b1a2c77853bc92437e5cb602f2d15ba
orsoyku2 <oykuors1098@gmail.com>
2021-03-03 23:10:02 +0300

    firts commit

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+import Directory from '../../components/directory/index'
+
+const HomePage = () => {
+      return (<div className="homepage">
+            <Directory/>
+      
+      </div>)
+     
+}
+export default HomePage;
