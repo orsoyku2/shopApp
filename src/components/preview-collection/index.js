54beac5183cfcea1991a63507c7360ca1168fd00
orsoyku2 <oykuors1098@gmail.com>
2021-04-11 13:47:20 +0300

    collectionItem component added into the previewcollection component

--- src/components/preview-collection/index.js
+++ src/components/preview-collection/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import CollectionItem from '../collection-item/index'
+import './styles.scss'
 
 const previewCollection = ({title,items}) => {
       return (
             <div className='collection-preview'>
                   <h1 className='title'>{title.toUpperCase()}</h1>
                   <div className='preview'>
-                        {items.filter((item,idx) => idx<4 ).map(item => (item.name))}
+                        {items
+                              .filter((item, idx) => idx < 4)
+                              .map(({ id, ...otherItemProps }) => (
+                                    <CollectionItem key={id} {...otherItemProps} />
+                              ))}
+                                    
+                                  
                   </div>
                   
             </div>
