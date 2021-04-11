4a03154482967811471d765cb7b90cfd40090567
orsoyku2 <oykuors1098@gmail.com>
2021-04-11 13:34:24 +0300

    collection-preview and collection-item component's css was created

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import './styles.scss'
 import Directory from '../../components/directory/index'
-import {Switch, Route, Link} from 'react-router-dom'
 const HomePage = ( props ) => {
       console.log(props.match.params)
       return (<div className="homepage">
             <Directory />
-            <button onClick={() => props.history.push('/hats')}>HATS</button>
-         
-      
       </div>)
      
 }
