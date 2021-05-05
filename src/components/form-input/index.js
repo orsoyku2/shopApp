f2c196f9f469f08ff30d2b84dab797177cf725dc
orsoyku2 <oykuors1098@gmail.com>
2021-05-05 20:07:07 +0300

    sign in component added

--- src/components/form-input/index.js
+++ src/components/form-input/index.js
@@ -3,14 +3,7 @@ import './styles.scss'
 
 const FormInput = () => {
     return <div className='group'>
-            <form onSubmit = {this.handleSubmit}>
-                              <input onChange={this.handleChange} type="email" name="email" required value={email}></input>
-                              <label>Email</label>
-                              <input onChange={this.handleChange} type="password" name="password" value={password} required></input>
-                              <label>Password</label>
-                              <input type="submit" value="Submit" />
-                             
-                        </form>
+    
 
     </div>
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -16,11 +16,7 @@ const Header = () => {
               Contact
             </Link>
             <Link class="option" to="/signin">
-<<<<<<< HEAD
               Sign In
-=======
-              Sign in
->>>>>>> abd7b59f9aad2ed73d06df0fe9c26bd32a6b3e5b
             </Link>
           </div>
         </div>
